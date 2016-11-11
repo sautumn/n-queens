c1fb45df5490eb8ef9800c268535e6dda84e5620
sautumn <autumn07@gmail.com>
2016-11-10 16:48:08 -0800

    tried different stuff for a complete nrook solution

--- src/solvers.js
+++ src/solvers.js
@@ -19,7 +19,99 @@ window.findNRookSolution = function (startingRow, startingCol, n) {
   //add 1 to position
   newBoard.togglePiece(startingRow, startingCol);
   var rookCount = 1;
-  
+  var solutionBoard = [];
+
+  var getNextCoordinate = function (board, currentRow, currentColumn) {
+    var n = board.get('n');
+    var firstFlag = true;
+    for (var i = currentRow; i < n; i++) {
+      for (var j = 0; j < n; j++) {
+        if (firstFlag) {
+          j = currentColumn;
+          firstFlag = false;
+        }
+        if (board.rows()[i][j] !== 1) {
+          return [i, j];
+        }
+      } 
+    }
+    return false;
+  };
+
+
+  var decisionTree = function(i, j, rookCount) {
+    if (i !== startingRow || j !== startingCol) {
+      newBoard.togglePiece(i, j);  
+    } 
+    if (rookCount === n) {
+      solutionBoard.push(newBoard);
+      rookCount = 0;
+      return null;
+    } else if (newBoard.hasAnyRooksConflicts()) {
+      //has a rook conflict, need to get next x,y
+      var nextCoordinate = getNextCoordinate(newBoard, i, j, rookCount);
+      newBoard.togglePiece(i, j);
+      if (nextCoordinate !== false) {
+        decisionTree(nextCoordinate[0], nextCoordinate[1], rookCount);
+      }
+      if (rookCount === n) {
+        solutionBoard.push(newBoard);
+        rookCount = 0;
+        return null;
+      }
+    } else {
+      //no rook conflict, increment counter
+      rookCount++;
+      var nextCoordinate = getNextCoordinate(newBoard, i, j);
+      if (nextCoordinate !== false) {
+        decisionTree(nextCoordinate[0], nextCoordinate[1], rookCount);
+      }
+      if (rookCount === n) {
+        solutionBoard.push(newBoard);
+        rookCount = 0;
+        return null;
+      }
+
+    }
+    if (nextCoordinate !== false) {
+      decisionTree(nextCoordinate[0], nextCoordinate[1], rookCount);
+    }
+  };
+
+
+  var decisionTree = function(i, j, rookCount) {
+
+    for (var x = 0; x < n; x++) {
+      for (var y = 0; y < n; y++) {
+        if (i !== startingRow || j !== startingCol) {
+          newBoard.togglePiece(i, j);
+        }
+        if (newBoard.hasAnyRooksConflicts()) {
+          newBoard.togglePiece(i, j);
+        } else {
+          rookCount++;
+          var nextCoordinate = getNextCoordinate(newBoard, i, j);
+          if (nextCoordinate !== false) {
+            decisionTree(nextCoordinate[0], nextCoordinate[1], rookCount);
+          }
+          if (rookCount === n) {
+            solutionBoard.push(newBoard);
+          }
+        }
+        var nextCoordinate = getNextCoordinate(newBoard, i, j);
+        if (nextCoordinate !== false) {
+          decisionTree(nextCoordinate[0], nextCoordinate[1], rookCount);
+        }
+      }
+    }
+  };
+
+  var rookCount = 0;
+  debugger;
+  decisionTree(0, 0, rookCount);
+  return solutionBoard;
+  /*
+  var solutionBoard = []; 
   for (var i = 0; i < n; i++) {
     for (var j = 0; j < n; j++) {
       if (i !== startingRow || j !== startingCol) {
@@ -31,22 +123,27 @@ window.findNRookSolution = function (startingRow, startingCol, n) {
         }
       }
       if (rookCount === n) {
-        return newBoard;
+        solutionBoard.push(newBoard);
       }
     }
-
-  }
+*/ 
 };
 
-window.findNRooksSolution = function(n) {
 
+
+window.findNRooksSolution = function(n) {
+/*
   var solutionArr = [];
   for (var i = 0; i < n; i++) {
     for (var j = 0; j < n; j++) {
       solutionArr.push(findNRookSolution(i, j, n).rows());
     }
   }
-  
+  */
+  //debugger;
+  var solution = findNRookSolution(0, 0, 2);
+  console.log(solution[0].rows()); 
+  /*
   solutionArr = _.uniq(solutionArr);
   var solutionArrString = _.map(solutionArr, function(boardArr) {
     return JSON.stringify(boardArr);
@@ -59,6 +156,7 @@ window.findNRooksSolution = function(n) {
 
   console.log(solutionArr);
   return solutionArr[0];
+  */
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
